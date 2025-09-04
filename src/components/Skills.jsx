ccfddc4787f3925f6f3ede609cebbd9613f44af6
Manish Ambre <manishambre5@gmail.com>
2025-09-04 10:43:18 +0200

    UI edit

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,18 +2,18 @@ import { FaArrowDown, FaPython, FaReact } from "react-icons/fa6";
 
 function Skills() {
     return (
-        <section id='Skills' className='shrink-0 pb-20 h-full flex flex-col gap-2 md:gap-4 snap-start px-4 md:px-2 bg-black justify-around items-center'>
+        <section id='Skills' className='shrink-0 pb-20 h-full flex flex-col gap-2 md:gap-4 snap-start px-4 md:px-2 bg-black justify-between lg:justify-around items-center'>
             <header className="shrink-0">
                 <h1 className='font-mono text-white text-center'>SKILLS</h1>
             </header>
-            <section className="shrink-0 grid md:grid-flow-col gap-4 items-center justify-center">
-                <article className='flex flex-col justify-start items-center md:items-start gap-2 md:gap-4 bg-white font-mono h-full max-w-lg p-4 inset-shadow'>
-                    <header className="flex gap-2 md:gap-4 justify-start items-center">
+            <section className="shrink-0 grid lg:grid-flow-col gap-4 items-center justify-center">
+                <article className='flex flex-col justify-start items-center lg:items-start gap-2 lg:gap-4 bg-white font-mono h-full max-w-lg p-4 inset-shadow'>
+                    <header className="flex gap-2 lg:gap-4 justify-start items-center">
                         <FaReact className="logo text-3xl" />
                         <h2 className="sm:text-2xl">Programming & Web Development</h2>
                     </header>
                     <h3 className="font-bold">Tech Stack</h3>
-                    <ul className="flex gap-2 sm:gap-4 flex-wrap justify-center md:justify-start">
+                    <ul className="flex gap-2 sm:gap-3 flex-wrap justify-center lg:justify-start">
                         <li className="keyword">React JS</li>
                         <li className="keyword">TailwindCSS</li>
                         <li className="keyword">HTML5</li>
@@ -36,13 +36,13 @@ function Skills() {
                         <li className="keyword">PostgreSQL</li>
                     </ul>
                 </article>
-                <article className='flex flex-col justify-start items-center md:items-start gap-2 md:gap-4 bg-white font-mono h-full max-w-lg p-4 inset-shadow'>
-                    <header className="flex gap-2 md:gap-4 justify-start items-center">
+                <article className='flex flex-col justify-start items-center lg:items-start gap-2 lg:gap-4 bg-white font-mono h-full max-w-lg p-4 inset-shadow'>
+                    <header className="flex gap-2 lg:gap-4 justify-start items-center">
                         <FaPython className="logo text-3xl" />
                         <h2 className="sm:text-2xl">Data Science & Analytics</h2>
                     </header>
                     <h3 className="font-bold">Tech Stack</h3>
-                    <ul className="flex gap-2 sm:gap-4 flex-wrap justify-center md:justify-start">
+                    <ul className="flex gap-2 sm:gap-3 flex-wrap justify-center lg:justify-start">
                         <li className="keyword">Pandas</li>
                         <li className="keyword">Matplotlib</li>
                         <li className="keyword">Scikit-learn</li>
