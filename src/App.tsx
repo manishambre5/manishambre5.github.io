662b2faa45dc1fd7fd007de732d3c7b06523ce2a
Manish Ambre <manishambre5@gmail.com>
2026-04-03 12:35:47 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Hero } from './components/Hero'
+import { MainContent } from './components/MainContent'
+
+function App() {
+
+  return (
+    <>
+    <Hero />
+    <MainContent />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/MainContent.tsx
@@ -0,0 +1,5 @@
+export const MainContent = () => {
+  return (
+    <div>MainContent</div>
+  )
+}
